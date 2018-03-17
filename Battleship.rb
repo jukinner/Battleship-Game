414396c8a49ae4354243d98b7d9fe45183ee0a35
Jukinner <jukinner@gmail.com>
2018-03-17 16:40:52 -0600

    Initial Battleship Terminal Game Push

---
+++ Battleship.rb
@@ -0,0 +1,309 @@
+@computer = { "A1" => "~", "A2" => "~", "A3" => "~", "A4" => "~", "A5" => "~", "A6" => "~", "A7" => "~", "A8" => "~", "A9" => "~", "A10" => "~", "B1" => "~", "B2" => "~", "B3" => "~", "B4" => "~", "B5" => "~", "B6" => "~", "B7" => "~", "B8" => "~", "B9" => "~", "B10" => "~", "C1" => "~", "C2" => "~", "C3" => "~", "C4" => "~", "C5" => "~", "C6" => "~", "C7" => "~", "C8" => "~", "C9" => "~", "C10" => "~", "D1" => "~", "D2" => "~", "D3" => "~", "D4" => "~", "D5" => "~", "D6" => "~", "D7" => "~", "D8" => "~", "D9" => "~", "D10" => "~", "E1" => "~", "E2" => "~", "E3" => "~", "E4" => "~", "E5" => "~", "E6" => "~", "E7" => "~", "E8" => "~", "E9" => "~", "E10" => "~","F1" => "~", "F2" => "~", "F3" => "~", "F4" => "~", "F5" => "~", "F6" => "~", "F7" => "~", "F8" => "~", "F9" => "~", "F10" => "~", "G1" => "~", "G2" => "~", "G3" => "~", "G4" => "~", "G5" => "~", "G6" => "~", "G7" => "~", "G8" => "~", "G9" => "~", "G10" => "~", "H1" => "~", "H2" => "~", "H3" => "~", "H4" => "~", "H5" => "~", "H6" => "~", "H7" => "~", "H8" => "~", "H9" => "~", "H10" => "~", "I1" => "~", "I2" => "~", "I3" => "~", "I4" => "~", "I5" => "~", "I6" => "~", "I7" => "~", "I8" => "~", "I9" => "~", "I10" => "~", "J1" => "~", "J2" => "~", "J3" => "~", "J4" => "~", "J5" => "~", "J6" => "~", "J7" => "~", "J8" => "~", "J9" => "~", "J10" => "~"}
+@shot_board = { "A1" => "~", "A2" => "~", "A3" => "~", "A4" => "~", "A5" => "~", "A6" => "~", "A7" => "~", "A8" => "~", "A9" => "~", "A10" => "~", "B1" => "~", "B2" => "~", "B3" => "~", "B4" => "~", "B5" => "~", "B6" => "~", "B7" => "~", "B8" => "~", "B9" => "~", "B10" => "~", "C1" => "~", "C2" => "~", "C3" => "~", "C4" => "~", "C5" => "~", "C6" => "~", "C7" => "~", "C8" => "~", "C9" => "~", "C10" => "~", "D1" => "~", "D2" => "~", "D3" => "~", "D4" => "~", "D5" => "~", "D6" => "~", "D7" => "~", "D8" => "~", "D9" => "~", "D10" => "~", "E1" => "~", "E2" => "~", "E3" => "~", "E4" => "~", "E5" => "~", "E6" => "~", "E7" => "~", "E8" => "~", "E9" => "~", "E10" => "~","F1" => "~", "F2" => "~", "F3" => "~", "F4" => "~", "F5" => "~", "F6" => "~", "F7" => "~", "F8" => "~", "F9" => "~", "F10" => "~", "G1" => "~", "G2" => "~", "G3" => "~", "G4" => "~", "G5" => "~", "G6" => "~", "G7" => "~", "G8" => "~", "G9" => "~", "G10" => "~", "H1" => "~", "H2" => "~", "H3" => "~", "H4" => "~", "H5" => "~", "H6" => "~", "H7" => "~", "H8" => "~", "H9" => "~", "H10" => "~", "I1" => "~", "I2" => "~", "I3" => "~", "I4" => "~", "I5" => "~", "I6" => "~", "I7" => "~", "I8" => "~", "I9" => "~", "I10" => "~", "J1" => "~", "J2" => "~", "J3" => "~", "J4" => "~", "J5" => "~", "J6" => "~", "J7" => "~", "J8" => "~", "J9" => "~", "J10" => "~"}
+@player = { "A1" => "~", "A2" => "~", "A3" => "~", "A4" => "~", "A5" => "~", "A6" => "~", "A7" => "~", "A8" => "~", "A9" => "~", "A10" => "~", "B1" => "~", "B2" => "~", "B3" => "~", "B4" => "~", "B5" => "~", "B6" => "~", "B7" => "~", "B8" => "~", "B9" => "~", "B10" => "~", "C1" => "~", "C2" => "~", "C3" => "~", "C4" => "~", "C5" => "~", "C6" => "~", "C7" => "~", "C8" => "~", "C9" => "~", "C10" => "~", "D1" => "~", "D2" => "~", "D3" => "~", "D4" => "~", "D5" => "~", "D6" => "~", "D7" => "~", "D8" => "~", "D9" => "~", "D10" => "~", "E1" => "~", "E2" => "~", "E3" => "~", "E4" => "~", "E5" => "~", "E6" => "~", "E7" => "~", "E8" => "~", "E9" => "~", "E10" => "~","F1" => "~", "F2" => "~", "F3" => "~", "F4" => "~", "F5" => "~", "F6" => "~", "F7" => "~", "F8" => "~", "F9" => "~", "F10" => "~", "G1" => "~", "G2" => "~", "G3" => "~", "G4" => "~", "G5" => "~", "G6" => "~", "G7" => "~", "G8" => "~", "G9" => "~", "G10" => "~", "H1" => "~", "H2" => "~", "H3" => "~", "H4" => "~", "H5" => "~", "H6" => "~", "H7" => "~", "H8" => "~", "H9" => "~", "H10" => "~", "I1" => "~", "I2" => "~", "I3" => "~", "I4" => "~", "I5" => "~", "I6" => "~", "I7" => "~", "I8" => "~", "I9" => "~", "I10" => "~", "J1" => "~", "J2" => "~", "J3" => "~", "J4" => "~", "J5" => "~", "J6" => "~", "J7" => "~", "J8" => "~", "J9" => "~", "J10" => "~"}
+@com_shot_board = { "A1" => "~", "A2" => "~", "A3" => "~", "A4" => "~", "A5" => "~", "A6" => "~", "A7" => "~", "A8" => "~", "A9" => "~", "A10" => "~", "B1" => "~", "B2" => "~", "B3" => "~", "B4" => "~", "B5" => "~", "B6" => "~", "B7" => "~", "B8" => "~", "B9" => "~", "B10" => "~", "C1" => "~", "C2" => "~", "C3" => "~", "C4" => "~", "C5" => "~", "C6" => "~", "C7" => "~", "C8" => "~", "C9" => "~", "C10" => "~", "D1" => "~", "D2" => "~", "D3" => "~", "D4" => "~", "D5" => "~", "D6" => "~", "D7" => "~", "D8" => "~", "D9" => "~", "D10" => "~", "E1" => "~", "E2" => "~", "E3" => "~", "E4" => "~", "E5" => "~", "E6" => "~", "E7" => "~", "E8" => "~", "E9" => "~", "E10" => "~","F1" => "~", "F2" => "~", "F3" => "~", "F4" => "~", "F5" => "~", "F6" => "~", "F7" => "~", "F8" => "~", "F9" => "~", "F10" => "~", "G1" => "~", "G2" => "~", "G3" => "~", "G4" => "~", "G5" => "~", "G6" => "~", "G" => "~", "G8" => "~", "G9" => "~", "G10" => "~", "H1" => "~", "H2" => "~", "H3" => "~", "H4" => "~", "H5" => "~", "H6" => "~", "H7" => "~", "H8" => "~", "H9" => "~", "H10" => "~", "I1" => "~", "I2" => "~", "I3" => "~", "I4" => "~", "I5" => "~", "I6" => "~", "I7" => "~", "I8" => "~", "I9" => "~", "I10" => "~", "J1" => "~", "J2" => "~", "J3" => "~", "J4" => "~", "J5" => "~", "J6" => "~", "J7" => "~", "J8" => "~", "J9" => "~", "J10" => "~"}
+
+# Computer sets up their board
+def comp_select
+
+  selection = rand(1..3)
+
+
+  case selection
+    when 1
+      # Aircraft Carrier
+          @computer['B1'] = 'A'
+          @computer['B2'] = 'A'
+          @computer['B3'] = 'A'
+          @computer['B4'] = 'A'
+          @computer['B5'] = 'A'
+
+    # Battleship
+          @computer['H5'] = 'B'
+          @computer['H6'] = 'B'
+          @computer['H7'] = 'B'
+          @computer['H8'] = 'B'
+
+    # Cruiser
+          @computer['E4'] = 'C'
+          @computer['F4'] = 'C'
+          @computer['G4'] = 'C'
+
+    # Submarine
+          @computer['J1'] = 'S'
+          @computer['J2'] = 'S'
+          @computer['J3'] = 'S'
+
+   # Destroyer
+          @computer['D8'] = 'D'
+          @computer['E8'] = 'D'
+
+
+    when 2
+      # Aircraft Carrier
+          @computer['E3'] = 'A'
+          @computer['E4'] = 'A'
+          @computer['E5'] = 'A'
+          @computer['E6'] = 'A'
+          @computer['E7'] = 'A'
+
+    # Battleship
+          @computer['A5'] = 'B'
+          @computer['B5'] = 'B'
+          @computer['C5'] = 'B'
+          @computer['D5'] = 'B'
+
+    # Cruiser
+          @computer['G1'] = 'C'
+          @computer['G2'] = 'C'
+          @computer['G3'] = 'C'
+
+    # Submarine
+          @computer['F9'] = 'S'
+          @computer['G9'] = 'S'
+          @computer['H9'] = 'S'
+
+   # Destroyer
+          @computer['I4'] = 'D'
+          @computer['I5'] = 'D'
+
+    when 3
+      # Aircraft Carrier
+          @computer['G3'] = 'A'
+          @computer['G4'] = 'A'
+          @computer['G5'] = 'A'
+          @computer['G6'] = 'A'
+          @computer['G7'] = 'A'
+
+    # Battleship
+          @computer['E2'] = 'B'
+          @computer['F2'] = 'B'
+          @computer['G2'] = 'B'
+          @computer['H2'] = 'B'
+
+    # Cruiser
+          @computer['G1'] = 'C'
+          @computer['G2'] = 'C'
+          @computer['G3'] = 'C'
+
+    # Submarine
+          @computer['B7'] = 'S'
+          @computer['C7'] = 'S'
+          @computer['D7'] = 'S'
+
+   # Destroyer
+          @computer['F9'] = 'D'
+          @computer['F10'] = 'D'
+
+
+end
+end
+
+# The user picks a coordinate on the board
+def input
+  coordinates = gets.chomp.upcase
+  if coordinates =~ /\A[A-J][0-9]\z|\A[A-J]10\z/
+    coordinates
+  else
+    puts "Wrong input"
+    puts "Please write input in the form of LetterNumber"
+    puts "Use A-J and 1-10"
+    input
+  end
+end
+
+# The computer picks a random coordinate
+def comp_input where
+  p "Computer has shot #{where}"
+end
+
+def show_board grid
+  puts "  # | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 "
+  letters = %w(A B C D E F G H I J)
+  counter = 1
+    letters.each do |letter|
+      puts "  #{letter} | #{grid["#{letter}1"]} | #{grid["#{letter}2"]} | #{grid["#{letter}3"]} | #{grid["#{letter}4"]} | #{grid["#{letter}5"]} | #{grid["#{letter}6"]} | #{grid["#{letter}7"]} | #{grid["#{letter}8"]} | #{grid["#{letter}9"]} | #{grid["#{letter}10"]} "
+      counter += 1
+    end
+end
+
+# returns true if shot is a hit
+def hit? which_board, coordinates
+  shot(which_board, coordinates) 
+  comp_input coordinates if which_board == @player
+  @com_shot_board.delete(coordinates) if which_board == @player
+  puts "hit" if which_board["#{coordinates}"] =~ /[ABCDS]/ 
+  true if which_board["#{coordinates}"] =~ /[X]/
+end
+
+def shot which_board, coordinates
+  if which_board == @computer
+    @computer["#{coordinates}"] =~ /[ABCDS]/ ? @shot_board["#{coordinates}"] = "X" : @shot_board["#{coordinates}"] = "O"
+    @computer["#{coordinates}"] =~ /[ABCDS]/ ? @computer["#{coordinates}"] = "X" : @computer["#{coordinates}"] = "O"
+  end
+  which_board["#{coordinates}"] =~ /[ABCDS]/ ? which_board["#{coordinates}"] = "X" : which_board["#{coordinates}"] = "O"
+end
+
+
+def cpu_turn
+  narrator("17")
+  which_sunk(hit?(@player, @com_shot_board.keys.sample))
+  puts "Computer Shot Board"
+  show_board @player
+  declare_winner
+  player_turn if !@game_end
+end
+
+def player_turn
+  narrator("18")
+  which_sunk(hit?(@computer, input))
+  puts "Computer Shot Board"
+  show_board @shot_board
+  declare_winner
+  cpu_turn if !@game_end
+end
+
+
+# UI PROMPTS
+@user_country = ""
+@cpu_country = ""
+
+def narrator(script_line_num)
+  prompt = {
+    '1' => "Welcome to Battle ship",
+    '2' => "You know how to play, right?",
+    '3' => "pick your country",
+    '4' => "pick your opponent's country",
+    '5' => "here is the board",
+    '6' => "let's place your ships",
+    '9' => "your opponent #{@cpu_country} has set up their board",
+    '10' => "#{@user_country} and #{@cpu_country} are at war!",
+    '13' => "where will you fire?",
+    '14' => "hit",
+    '15' => "miss",
+    '16' => "here is the board",
+    '17' => "it is #{@cpu_country}'s turn",
+    '18' => "it is #{@user_country}'s turn",
+    '19' => "they fire",
+    '20' => "hit",
+    '21' => "miss",
+    '22' => "it is your turn",
+    '23' => "Somebody wins",
+    '24' => "game over",
+    '25' => "do you want to play again?",
+    '26' => "You sunk a ship!"
+  }
+  p prompt[script_line_num]
+end
+
+
+
+# CODE FOR DETERIMING IF A HIT SUNK A SHIP AND WHAT SHIP IT WAS FOR COMPUTER AND PERSON.
+# REQUIRES THESE VARIABLES
+@computer_sunk_ships = []
+@player_sunk_ships = []
+@player_current_sunk_ship = []
+@computer_current_sunk_ship = []
+
+
+
+def which_sunk result_of_attempt
+  ship_names = ["A", "B", "C", "D", "S"]
+  ships = {  "A" => "Aircraft Carrier", "B" => "Battleship", "C" => "Carrier","D" => "Destroyer", "S" => "Submarine" }
+  i = 0
+  c = 0
+  if result_of_attempt == true
+    5.times do
+      if @player.has_value?(ship_names[i]) == false && @player_sunk_ships.include?(ships.fetch_values(ship_names[i])) == false
+        @player_sunk_ships << ships.fetch_values(ship_names[i])
+        @player_current_sunk_ship << ships.fetch_values(ship_names[i])
+        puts "You sunk a ship!:"
+        puts @player_current_sunk_ship
+        puts "And the total ships sunk is:"
+        puts @player_sunk_ships
+      end
+    i = i + 1
+    end
+    5.times do
+      if @computer.has_value?(ship_names[c]) == false && @computer_sunk_ships.include?(ships.fetch_values(ship_names[c])) == false
+        @computer_sunk_ships << ships.fetch_values(ship_names[c])
+        @computer_current_sunk_ship << ships.fetch_values(ship_names[c])
+        puts "The computer sunk a ship:"
+        puts @computer_current_sunk_ship
+        puts "And the total computer ships sunk is:"
+        puts @computer_sunk_ships
+      end
+    c = c + 1
+    end
+  end
+end
+
+def player_select
+
+  loopcount = 5
+  counter = 1
+
+  while counter <= loopcount
+    system "clear"
+    show_board @player
+  @choosing = 'Aircraft Carier' if counter == 1
+  @choosing = 'Battleship' if counter == 2
+  @choosing = 'Cruiser' if counter == 3
+  @choosing = 'Submarine' if counter == 4
+  @choosing = 'Destroyer' if counter == 5
+
+  selections = 5 if counter == 1
+  selections = 4 if counter == 2
+  selections = 3 if counter == 3
+  selections = 3 if counter == 4
+  selections = 2 if counter == 5
+
+  letter = "A" if counter == 1
+  letter = "B" if counter == 2
+  letter = "C" if counter == 3
+  letter = "S" if counter == 4
+  letter = "D" if counter == 5
+
+  select_counter = 0
+    while select_counter < selections
+      puts "Select a location for your #{@choosing}"
+      @choice = input
+      @player["#{@choice}"] = letter
+      show_board @player
+    select_counter = select_counter + 1
+    end
+  counter = counter + 1
+  end
+
+end
+
+# DETERMINING AND DECLARING A WINNER- SIMPLY CHECKS THE SUNK SHIP ARRAYS FOR BOTH PLAYERS FOR A COUNT OF 5. 
+
+def declare_winner
+  if @player_sunk_ships.count == 5
+    puts "#{@user_country} wins! Player 1 wins! "
+    @game_end = true
+  elsif @computer_sunk_ships.count == 5
+    puts "#{@cpu_country} wins! Computer wins!"
+    @game_end = true
+  end
+end
+
+def start
+  @game_end = false
+  narrator('1')
+  narrator('3')
+  @user_country = gets.chomp
+  narrator('4')
+  @cpu_country = gets.chomp
+  narrator('5')
+  show_board @player
+  narrator('6')
+  player_select
+  comp_select
+  narrator('9')
+  narrator('10')
+  player_turn
+end
+
+start
